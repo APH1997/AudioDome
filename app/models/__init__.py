acce91ad677091f724170db36b3b3eaac71ced18
Bao <bao4ltyfe@gmail.com>
2023-05-05 14:20:15 -0700

    song seeders 70% done probably

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from .db import db
 from .user import User
 from .playlist import Playlist
 from .PlaylistSongs import Playlist_Song
-from .song import Songs
+from .song import Song
 from .SongLikes import Song_Like
 from .db import environment, SCHEMA
---
+++ app/seeds/songs.py
@@ -0,0 +1,32 @@
+from app.models import db, User, environment, SCHEMA, Song
+from sqlalchemy.sql import text
+
+def seed_songs():
+    song1 = Song(
+        title='Song One',
+        artist='Song Artist One',
+        aws_url='Amazon One',
+        uploader_id=1
+    )
+    song2 = Song(
+        title='Song Two',
+        artist='Song Artist Two',
+        aws_url='Amazon Two',
+        uploader_id=2
+    )
+    song3 = Song(
+        title='Song Three',
+        artist='Song Artist Three',
+        aws_url='Amazon Three',
+        uploader_id=3
+    )
+
+    all_songs = [song1, song2, song3]
+    add_songs = [db.session.add(song) for song in all_songs]
+    db.session.commit()
+
+def undo_songs():
+    db.session.execute(
+        text('DELETE FROM songs')
+    )
+    db.session.commit()
