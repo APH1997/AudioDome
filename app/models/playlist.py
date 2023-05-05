c4eddd24b5267d3c544acca784432cc824bd521d
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-05 11:02:34 -0700

    removed usermixen from line 6

--- app/models/playlist.py
+++ app/models/playlist.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 
 
-class Playlist(db.Model, UserMixin):
+class Playlist(db.Model):
     __tablename__ = 'playlists'
 
     if environment == "production":
