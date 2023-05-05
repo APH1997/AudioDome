7f36f7dc49f6dabfba9a1c38e7e607b31b1a9edc
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-05 15:15:19 -0400

    added the ForeignKey

--- app/models/PlaylistSongs.py
+++ app/models/PlaylistSongs.py
@@ -4,5 +4,5 @@ from .db import db
 class Playlist_Song():
     __tablename__= 'Playlist_Songs'
 
-    song_id = db.Column(db.Integer)
-    playlist_id =db.Column(db.Integer)
+    song_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
+    playlist_id =db.Column(db.Integer, db.ForeignKey('playlists.id'))
