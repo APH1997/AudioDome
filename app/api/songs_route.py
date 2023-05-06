558f8408c8c7483f72e84d536e07373d6b8084ea
Bao <bao4ltyfe@gmail.com>
2023-05-06 11:55:40 -0700

    did basic route stuff for songs

--- app/api/songs_route.py
+++ app/api/songs_route.py
@@ -1,3 +1,21 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from app.models import Song, db
 
 song_routes = Blueprint('auth',__name__)
+
+@song_routes.route('/')
+def get_all_song():
+    songs = Song.query.all()
+    songs_list = [song.to_dict() for song in songs]
+    return jsonify(songs_list)
+
+
+@song_routes.route('/<int:id>')
+def get_song_by_id(id):
+    song = Song.query.get(id)
+    return song.to_dict()
+
+
+@song_routes.route('/<int:id>', methods=['PUT'])
+def edit_song_by_id(id):
+    song = Song.query.get(id)
