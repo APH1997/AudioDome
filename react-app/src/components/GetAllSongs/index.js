2830ecc317ffae17745f1332d1d43a7832e80935
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-06 17:13:59 -0400

    waiting for andre

--- react-app/src/components/GetAllSongs/index.js
+++ react-app/src/components/GetAllSongs/index.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
 import { getSongsThunk } from '../../store/songs';
 import allsongs from "./allsongs.css"
 function GetAllSongs() {
     const dispatch = useDispatch()
     const allSongs = useSelector(state => state.songs)
-
+    console.log("HERE IS YOUR ALLSONGS", Object.values(allSongs))
     useEffect(() => {
         dispatch(getSongsThunk())
     }, [dispatch])
@@ -14,7 +13,10 @@ function GetAllSongs() {
         <div>
             {Object.values(allSongs).length > 0 && Object.values(allSongs).map(song =>
                 <>
-                    {`${song?.name}`}
+                <div>
+                    {`${song?.title}`}
+                </div>
+
                 </>
             )}
         </div>
