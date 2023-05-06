804896125e326c14b2c7ad1ae1fd7346cbfac7a7
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-06 15:33:15 -0400

    get all songs

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
+import GetAllSongs from "./components/GetAllSongs";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
 
@@ -18,12 +19,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
+          <Route exact path="/login"><LoginFormPage /></Route>
+          <Route exact path="/signup"><SignupFormPage /></Route>
+          <Route exact path="/"><GetAllSongs /></Route>
         </Switch>
       )}
     </>
