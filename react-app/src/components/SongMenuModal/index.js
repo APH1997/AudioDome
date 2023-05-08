9e035038c6acdca80125bddb77934d8e20e8eb8a
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-08 10:51:13 -0700

    redirection for update button

--- react-app/src/components/SongMenuModal/index.js
+++ react-app/src/components/SongMenuModal/index.js
@@ -3,12 +3,16 @@ import OpenModalButton from "../OpenModalButton";
 import { useState, useEffect, useRef } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import DeleteSong from '../DeleteSongModal/DeleteSong';
+import { useHistory } from 'react-router-dom';
+import {useModal} from '../../context/Modal'
 
 function SongMenu({ song }) {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef();
     const user = useSelector(state => state.session)
+    const history = useHistory()
+    const {closeModal} = useModal()
     const openMenu = () => {
         if (showMenu) return;
         setShowMenu(true);
@@ -32,6 +36,11 @@ function SongMenu({ song }) {
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
     const closeMenu = () => setShowMenu(false);
 
+    function handleUpdate(){
+        closeModal()
+        history.push('/')
+    }
+
     return (
         <>
             <ul>
@@ -45,7 +54,7 @@ function SongMenu({ song }) {
                                 onItemClick={closeMenu}
                                 modalComponent={<DeleteSong song={song} />}
                             />
-                            <button>Update Song</button>
+                            <button onClick={() => handleUpdate()}>Update Song</button>
                         </div>
                     </>
                 }
