51d5bdfdbe1626c5d453d241f0ac8c6484a9be0b
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-09 12:44:10 -0700

    fixed dev bug

--- react-app/src/components/AudioPlayer/AudioPlayer.js
+++ react-app/src/components/AudioPlayer/AudioPlayer.js
@@ -8,10 +8,7 @@ import { useSelector, useDispatch } from 'react-redux'
 const AudioPlayer = () => {
     const [currentSong, setCurrentSong] = useState([])
     const audioRef = useRef()
-    const songs = useSelector(state=> state.currentSong)
-    const songArray = Object.values(songs)
-    let counter = 0
-    setCurrentSong(songArray[counter])
+   
     return (
       <div>
         <div>
