fd5341676f5fb96583e3e6778df4c6e761fe5545
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-09 19:14:58 -0400

    auto play

--- react-app/src/components/AudioPlayer/AudioPlayer.js
+++ react-app/src/components/AudioPlayer/AudioPlayer.js
@@ -1,31 +1,52 @@
-import { getAllSongsThunk } from "../../store/currentSong";
 import Controls from "./Controls";
 import DisplayTrack from "./DisplayTrack";
 import ProgressBar from "./ProgressBar";
 import { useState, useRef, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-
+import { getAllSongsThunk } from "../../store/currentSong";
 
 const AudioPlayer = () => {
-    const dispatch = useDispatch()
-    const [currentSong, setCurrentSong] = useState(3)
-    const audioRef = useRef()
-    const songs = useSelector(state => state.currentSong)
-    useEffect(() => {
-      dispatch(getAllSongsThunk())
-    }, [dispatch])
+  const audioRef = useRef()
+  const dispatch = useDispatch()
+  const songs = useSelector(state => state.currentSong)
+  const [currentSongIndex, setCurrentSongIndex] = useState(0)
+  const songIds = Object.keys(songs)
 
-    const songId = 3
-    console.log('songs ==============>', songs)
+  useEffect(() => {
+    dispatch(getAllSongsThunk())
+  }, [dispatch])
 
-    return (
+  useEffect(() => {
+    const audio = audioRef.current
+    audio.play()
+    audio.addEventListener('ended', () => {
+      setCurrentSongIndex((currentSongIndex + 1) % songIds.length)
+      audio.currentTime = 0
+      audio.play()
+    })
+    return () => {
+      audio.removeEventListener('ended', () => {
+        setCurrentSongIndex((currentSongIndex + 1) % songIds.length)
+        audio.currentTime = 0
+        audio.play()
+      })
+    }
+  }, [audioRef, currentSongIndex, songIds])
+
+  const handleNextSong = () => {
+    setCurrentSongIndex((currentSongIndex + 1) % songIds.length)
+    audioRef.current.currentTime = 0
+    audioRef.current.play()
+  }
+
+  return (
+    <div>
       <div>
-        <div>
-            <DisplayTrack audioRef={audioRef} track={songs[currentSong]?.awsUrl}/>
-            <Controls audioRef={audioRef}/>
-            <ProgressBar />
-        </div>
+        <DisplayTrack audioRef={audioRef} track={songs[songIds[currentSongIndex]]?.awsUrl} />
+        <Controls audioRef={audioRef} handleNextSong={handleNextSong} />
+        <ProgressBar />
       </div>
-    );
-  };
-  export default AudioPlayer;
+    </div>
+  );
+};
+export default AudioPlayer;
