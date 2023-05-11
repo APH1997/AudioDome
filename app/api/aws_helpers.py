e709a01d922775fb185c8e93aa309836a868e4a2
Andre <andrehristu@gmail.com>
2023-05-11 14:10:28 -0400

    console logs to test s3 upload in prod

--- app/api/aws_helpers.py
+++ app/api/aws_helpers.py
@@ -36,6 +36,11 @@ def upload_file_to_s3(file, acl="public-read"):
                 "ContentType": file.content_type
             }
         )
+        print('S3 UPLOAD FUNC FILE', file)
+        print('S3 UPLOAD FUNC CONTENT TYPE', file.content_type)
+        print('S3 UPLOAD FUNC BUCKET_NAME', BUCKET_NAME)
+        print('S3 UPLOAD FUNC FILE.FILENAME', file.filename)
+        print('S3 UPLOAD FUNC ACL', acl)
     except Exception as e:
         # in case the our s3 upload fails
         return {"errors": str(e)}
