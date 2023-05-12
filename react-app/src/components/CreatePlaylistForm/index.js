3f5d4db6a487ba048167645ae29226b8c672835b
Bao <bao4ltyfe@gmail.com>
2023-05-12 10:48:38 -0700

    validstion

--- react-app/src/components/CreatePlaylistForm/index.js
+++ react-app/src/components/CreatePlaylistForm/index.js
@@ -14,10 +14,8 @@ const PlaylistForm = () => {
     // console.log(checked,'work ~~~~~~~~~~~~~~');
     const [name, setName] = useState('')
     const [imgFile, setImageFile] = useState(null)
+    const [error, setError] = useState(null)
     const allSongs = Object.values(allSongsObj)
-    console.log(user, 'This is the User');
-    console.log(allSongs, 'SONGSSSSS');
-
 
     useEffect(() => {
         dispatch(getSongsThunk())
@@ -33,19 +31,33 @@ const PlaylistForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        if (!imgFile){
+            setError("Image is required")
+            return
+        }
+        if (!name) {
+            setError("Name is required");
+            return;
+        }
+        if (!checked.length) {
+            setError("Please select at least one song");
+            return;
+        }
+
         const formData = new FormData()
 
-        formData.append('user_id',user.id)
+        formData.append('user_id', user.id)
         formData.append('name', name)
         formData.append('playlist_image', imgFile)
         formData.append('playlist_songs', checked.join(','))
 
         await dispatch(createPlaylistThunk(formData))
         history.push('/')
+
     }
 
     const handelCheckBox = (e) => {
-        if (e.target.checked){
+        if (e.target.checked) {
             setChecked([
                 ...checked, e.target.value
             ])
@@ -58,22 +70,26 @@ const PlaylistForm = () => {
 
     return (
         <form onSubmit={handleSubmit}>
+            {error &&
+                <div className="error">
+                    {error}
+                </div>}
             <div className="topOfPage">
                 <div className="playlistImage">
                     <input id="playlistImages"
-                    type="file"
-                    name="playlistPicture"
-                    accept="image/*"
-                    onChange={handleAddImage} />
+                        type="file"
+                        name="playlistPicture"
+                        accept="image/*"
+                        onChange={handleAddImage} />
                 </div>
                 <label>
                     Name
                     <input
-                    id="playlistName"
-                    placeholder={`My Playlist #${(user.playlists).length + 1}`}
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}/>
+                        id="playlistName"
+                        placeholder={`My Playlist #${(user.playlists).length + 1}`}
+                        type="text"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} />
                 </label>
             </div>
             <div>
@@ -87,21 +103,21 @@ const PlaylistForm = () => {
                         </button>
                     </div>
                 )} */}
-                { allSongs.map(song =>
+                {allSongs.map(song =>
                     <div>
                         <label>
                             {song?.title}
                             <input
-                            type="checkbox"
-                            name='song'
-                            value={song?.id}
-                            onChange={handelCheckBox}
+                                type="checkbox"
+                                name='song'
+                                value={song?.id}
+                                onChange={handelCheckBox}
                             />
                         </label>
                     </div>)}
             </div>
             <div className='SubmitPlaylistBtn'>
-                    <button className="create-playlist-button" type="submit">Create Playlist</button>
+                <button className="create-playlist-button" type="submit">Create Playlist</button>
             </div>
         </form>
     )
--- react-app/src/components/CreateSongForm/index.js
+++ react-app/src/components/CreateSongForm/index.js
@@ -3,26 +3,27 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { createSongThunk, getSongsThunk } from '../../store/songs'
 
-function CreateSongForm(){
+function CreateSongForm() {
     const currentUser = useSelector(state => state.session.user);
     const songs = useSelector(state => state.songs)
     const songLength = Object.values(songs).length
     console.log(currentUser)
-    console.log('songs use selector',songs)
-    console.log('songs length',Object.values(songs).length)
+    console.log('songs use selector', songs)
+    console.log('songs length', Object.values(songs).length)
     const history = useHistory()
     const dispatch = useDispatch()
-    const [title , setTitle] = useState('');
+    const [title, setTitle] = useState('');
     const [artist, setArtist] = useState('');
     const [currSongLength, setCurrSongLength] = useState(songLength)
     const [file, setFile] = useState(null)
     const [imgFile, setImageFile] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         dispatch(getSongsThunk())
     }, [dispatch, songLength])
 
-    function handleFileUpload(e){
+    function handleFileUpload(e) {
         setFile(e.target.files[0])
     }
     const handleAddImage = (e) => {
@@ -31,11 +32,29 @@ function CreateSongForm(){
     /*----------------------ANY VALIDATION WOULD DO HERE AS WELL FOR THE FORM FOR HANDLESUBMIT-------------------- */
     const HandleSubmit = async (e) => {
         e.preventDefault()
+        if (!title) {
+            setError('Title is required')
+            return
+        }
+        if (!artist) {
+            setError('Artist is required')
+            return
+        }
+        if (!file) {
+            setError('Song is required')
+            return
+        }
+        if (imgFile) {
+            setError('Cover-Art is required')
+            return
+        }
+
+
         history.push('/songs/all')
         setCurrSongLength(+songLength)
         const formData = new FormData()
-        formData.append("title",title)
-        formData.append("artist",artist)
+        formData.append("title", title)
+        formData.append("artist", artist)
         formData.append("aws_url", file)
         formData.append("uploader_id", currentUser.id)
         formData.append('song_image', imgFile)
@@ -44,9 +63,13 @@ function CreateSongForm(){
     }
     return (
         <form onSubmit={HandleSubmit} encType="multipart/form-data">
+            {error &&
+                <div className="error">
+                    {error}
+                </div>}
             <label>
                 <div>Upload A Song</div>
-                <input id="song-upload" type="file" name="song" accept="audio/*" onChange={handleFileUpload}/>
+                <input id="song-upload" type="file" name="song" accept="audio/*" onChange={handleFileUpload} />
             </label>
             <label>
                 <div>Add Song Cover-Art</div>
@@ -61,7 +84,7 @@ function CreateSongForm(){
                 <input id="artist-name" type="text" value={artist} placeholder='Artist Time' onChange={(e) => setArtist(e.target.value)} />
             </label>
             <div className='SubmitSongBtn'>
-                    <button className="create-song-button" type="submit">Submit Song</button>
+                <button className="create-song-button" type="submit">Submit Song</button>
             </div>
         </form>
     )
