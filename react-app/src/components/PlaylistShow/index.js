ad61721c110ae2ccf11652e228941f9b590f9c3c
Domenik Moody <Domeniktmoody@gmail.com>
2023-05-13 15:47:27 -0400

    more css

--- react-app/src/components/PlaylistShow/index.js
+++ react-app/src/components/PlaylistShow/index.js
@@ -8,9 +8,6 @@ function PlaylistShow() {
     const dispatch = useDispatch()
     const { playlistId } = useParams()
     const singlePlaylistObj = useSelector(state => state.playlist.singlePlaylist)
-    // const singlePlaylist = Object.values(singlePlaylistObj)
-    // console.log(singlePlaylist);
-    console.log(singlePlaylistObj);
     useEffect(() => {
         dispatch(getOnePlaylistThunk(playlistId))
     }, [dispatch])
@@ -22,7 +19,6 @@ function PlaylistShow() {
 
     return (
         <div>
-            {/* hello */}
             {singlePlaylistObj.songs.map((song, index) => (
                 <div>
                     <SongCard song={song} number={index+1} playlistId={playlistId} />
