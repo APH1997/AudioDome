08dce87f55ac3975a231a2c259e6cdfe2da93d79
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-05-14 15:31:30 -0700

    more playlist seeds

--- app/seeds/playlist_seed.py
+++ app/seeds/playlist_seed.py
@@ -1,7 +1,11 @@
 from app.models import db, Playlist, environment, SCHEMA
 from sqlalchemy.sql import text
+from random import randint, sample, choice
 
 def seed_playlist(seeded_users, seeded_songs):
+
+    random_playlist_img = ["http://audiodome-songs.s3.amazonaws.com/33938413b56b4d9ca1be814fd3b3d906.jpg", "http://audiodome-songs.s3.amazonaws.com/9de87b09993648498937f8e1c6ccd1fe.jpg", "http://audiodome-songs.s3.amazonaws.com/60c77dde8e8f4699be314c9b2845681f.jpg", "http://audiodome-songs.s3.amazonaws.com/152d4801a81a4bfb870b12bee63b75c5.jpg", "http://audiodome-songs.s3.amazonaws.com/6bd28fa93d6747afb8f2e8340a83862d.jpg", "http://audiodome-songs.s3.amazonaws.com/32c3f02cecbc445f8c667310705eed9a.jpg", "http://audiodome-songs.s3.amazonaws.com/ad20d705d51f4f76b44032c746e374e9.jpg", "http://audiodome-songs.s3.amazonaws.com/25e050a19090485fb415a1258117e176.jpg", "http://audiodome-songs.s3.amazonaws.com/c5ef88ac42244d8aad7d1395029204a8.jpg"]
+
     Playlist1 = Playlist(
         user = seeded_users[0],
         name="Groove City",
@@ -38,6 +42,48 @@ def seed_playlist(seeded_users, seeded_songs):
         playlist_image="http://audiodome-songs.s3.amazonaws.com/f2a2a7398059443b9dd8f81031b83dc6.jpg",
         playlist_songs = [seeded_songs[40],seeded_songs[41],seeded_songs[42],seeded_songs[43]]
     )
+    Playlist7 = Playlist(
+        user = seeded_users[5],
+        name="TrillDaBeast",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist8 = Playlist(
+        user = seeded_users[5],
+        name="Mellow Bebops",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist9 = Playlist(
+        user = choice(seeded_users),
+        name="Bebop Beats",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist10 = Playlist(
+        user = choice(seeded_users),
+        name="Sonic Lagoon",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist11 = Playlist(
+        user = choice(seeded_users),
+        name="Drive Fast Music",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist12 = Playlist(
+        user = choice(seeded_users),
+        name="I can't pause it mom, it's ONLINE",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
+    Playlist13 = Playlist(
+        user = choice(seeded_users),
+        name="Bo's BoomBox",
+        playlist_image=choice(random_playlist_img),
+        playlist_songs = sample(seeded_songs, randint(0, len(seeded_songs)))
+    )
 
 
     db.session.add(Playlist1)
@@ -46,6 +92,13 @@ def seed_playlist(seeded_users, seeded_songs):
     db.session.add(Playlist4)
     db.session.add(Playlist5)
     db.session.add(Playlist6)
+    db.session.add(Playlist7)
+    db.session.add(Playlist8)
+    db.session.add(Playlist9)
+    db.session.add(Playlist10)
+    db.session.add(Playlist11)
+    db.session.add(Playlist12)
+    db.session.add(Playlist13)
     db.session.commit()
 
 def undo_playlist():
